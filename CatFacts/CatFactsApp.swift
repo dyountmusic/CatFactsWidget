705f67e3cbf0ad3c2381787270d097bf45d79a8d
Daniel Yount <yountdaniel@gmail.com>
2020-09-08 13:46:07 -0600

    Initial Commit

---
+++ CatFacts/CatFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatFactsApp.swift
+//  CatFacts
+//
+//  Created by Daniel Yount on 9/8/20.
+//
+
+import SwiftUI
+
+@main
+struct CatFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
